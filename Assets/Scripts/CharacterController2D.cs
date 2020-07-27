0b60e8b941da913e0864c6255de26666776afe7f
dgpMakes <43809495+dgpMakes@users.noreply.github.com>
2020-07-27 16:58:19 +0200

    Double Jump

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -47,27 +47,28 @@ public class CharacterController2D : MonoBehaviour {
 	bool facingRight = true;
 	bool isGrounded = false;
 	bool isRunning = false;
+	bool canDoubleJump = false;
 
 	// store the layer the player is on (setup in Awake)
 	int _playerLayer;
 
 	// number of layer that Platforms are on (setup in Awake)
 	int _platformLayer;
-	
-	void Awake () {
+
+	void Awake() {
 		// get a reference to the components we are going to be changing and store a reference for efficiency purposes
-		_transform = GetComponent<Transform> ();
-		
-		_rigidbody = GetComponent<Rigidbody2D> ();
-		if (_rigidbody==null) // if Rigidbody is missing
+		_transform = GetComponent<Transform>();
+
+		_rigidbody = GetComponent<Rigidbody2D>();
+		if (_rigidbody == null) // if Rigidbody is missing
 			Debug.LogError("Rigidbody2D component missing from this gameobject");
-		
+
 		_animator = GetComponent<Animator>();
-		if (_animator==null) // if Animator is missing
+		if (_animator == null) // if Animator is missing
 			Debug.LogError("Animator component missing from this gameobject");
-		
-		_audio = GetComponent<AudioSource> ();
-		if (_audio==null) { // if AudioSource is missing
+
+		_audio = GetComponent<AudioSource>();
+		if (_audio == null) { // if AudioSource is missing
 			Debug.LogWarning("AudioSource component missing from this gameobject. Adding one.");
 			// let's just add the AudioSource component dynamically
 			_audio = gameObject.AddComponent<AudioSource>();
@@ -88,10 +89,10 @@ public class CharacterController2D : MonoBehaviour {
 			return;
 
 		// determine horizontal velocity change based on the horizontal input
-		_vx = Input.GetAxisRaw ("Horizontal");
+		_vx = Input.GetAxisRaw("Horizontal");
 
 		// Determine if running based on the horizontal movement
-		if (_vx != 0) 
+		if (_vx != 0)
 		{
 			isRunning = true;
 		} else {
@@ -107,24 +108,28 @@ public class CharacterController2D : MonoBehaviour {
 		// Check to see if character is grounded by raycasting from the middle of the player
 		// down to the groundCheck position and see if collected with gameobjects on the
 		// whatIsGround layer
-		isGrounded = Physics2D.Linecast(_transform.position, groundCheck.position, whatIsGround);  
+		isGrounded = Physics2D.Linecast(_transform.position, groundCheck.position, whatIsGround);
+		//allow double jump
+		if (isGrounded)
+		{
+			canDoubleJump = true;
+		}
 
 		// Set the grounded animation states
 		_animator.SetBool("Grounded", isGrounded);
 
-		if(isGrounded && Input.GetButtonDown("Jump")) // If grounded AND jump button pressed, then allow the player to jump
+		if (isGrounded && Input.GetButtonDown("Jump")) // If grounded AND jump button pressed, then allow the player to jump
 		{
-			// reset current vertical motion to 0 prior to jump
-			_vy = 0f;
-			// add a force in the up direction
-			_rigidbody.AddForce (new Vector2 (0, jumpForce));
-			// play the jump sound
-			PlaySound(jumpSFX);
-		}
-	
+			DoJump();
+		}else if (canDoubleJump && Input.GetButtonDown("Jump"))
+        {
+			DoJump();
+			canDoubleJump = false;
+        }
+
 		// If the player stops jumping mid jump and player is not yet falling
 		// then set the vertical velocity to 0 (he will start to fall from gravity)
-		if(Input.GetButtonUp("Jump") && _vy>0f)
+		if (Input.GetButtonUp("Jump") && _vy > 0f)
 		{
 			_vy = 0f;
 		}
@@ -135,7 +140,16 @@ public class CharacterController2D : MonoBehaviour {
 		// if moving up then don't collide with platform layer
 		// this allows the player to jump up through things on the platform layer
 		// NOTE: requires the platforms to be on a layer named "Platform"
-		Physics2D.IgnoreLayerCollision(_playerLayer, _platformLayer, (_vy > 0.0f)); 
+		Physics2D.IgnoreLayerCollision(_playerLayer, _platformLayer, (_vy > 0.0f));
+	}
+
+	void DoJump (){
+		// reset current vertical motion to 0 prior to jump
+		_vy = 0f;
+		// add a force in the up direction
+		_rigidbody.AddForce(new Vector2(0, jumpForce));
+			// play the jump sound
+			PlaySound(jumpSFX);
 	}
 
 	// Checking to see if the sprite should be flipped
